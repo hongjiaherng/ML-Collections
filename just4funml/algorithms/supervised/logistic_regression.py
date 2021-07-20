191041c9cb5d965f26c7b946fdb489e4f7b16908
jiaherng <jiaherng2001@gmail.com>
2021-07-20 19:11:42 +0800

    Completed: Softmax Regression

--- just4funml/algorithms/supervised/logistic_regression.py
+++ just4funml/algorithms/supervised/logistic_regression.py
@@ -20,14 +20,12 @@ import just4funml.utils.preprocessing as preprocessing
 
 class SoftmaxRegression:
     def __init__(self, learning_rate=0.1,
-                 max_iter=100, regularized_type=None, alpha=None, early_stopping=False, 
-                 random_state=None):  # C is regularization is applied, which level of regularization, early stopping option
+                 max_iter=100, regularized_type=None, alpha=None, random_state=None):
         """
         learning_rate       : Learning rate of batch gradient descent
         max_iter            : Maximum iteration of gradient descent
         regularized_type    : [None, 'l2', 'l1'] Add penalty to the cost function of SoftmaxRegression, either 'l2 loss', 'l1 loss', or 'None'
         alpha               : Regularized hyperparameter (aka C), the lower alpha is, the more regularization is applied; alpha is ignored when regularized_type is None
-        early_stopping      : If True, gradient descent will stop as soon as the cost reaches a minimum
         random_state        : Set seed to numpy random
         """ 
 
@@ -38,11 +36,13 @@ class SoftmaxRegression:
         self.max_iter = max_iter
         self.regularized_type = regularized_type
         self.alpha = alpha
-        self.early_stopping = early_stopping
         np.random.seed(random_state)
 
     def fit(self, X, y):
-        
+        """
+        Fit the model with batch gradient descent
+        """
+
         # Validate dimension of X and y
         if X.ndim != 2:
             raise ValueError('X is expected to be a 2d numpy array')
@@ -51,59 +51,115 @@ class SoftmaxRegression:
         elif len(X) != len(y):
             raise ValueError('Number of rows of X must match with number of rows of y')
 
-        # Add bias term to X
+        # Prepare X and Y
         X_with_bias = np.c_[np.ones((len(X), 1)), X]    # (m x (n + 1))    
-        # One hot encode y
         Y_one_hot = preprocessing.to_one_hot(y)         # (m x k)
 
+        # Declare some useful variables
+        m = len(X)                                      # Number of training examples
         self.K = Y_one_hot.shape[1]                     # Number of classes
         self.n = X_with_bias.shape[1] - 1               # Number of features
-        m = len(X)                                      # Number of training examples
-
-        # Initialize theta randomly
         self.Theta = np.random.randn(self.n + 1, self.K)    # ((n + 1) x k)
-        
-        # To keep track of lowest cost when early_stopping is enabled
-        best_cost = np.inf  
-
+     
         # Run batch gradient descent for max_iter times
         # Gradient descent can be stopped by:
         #   - max_iter reached
-        #   - If early_stopping is enable, it stops when newly computed cost is higher than previous cost
-
         for epoch in range(self.max_iter):
 
-            # Compute softmax score / logit for each class according to each training example 
-            logits = X_with_bias @ self.Theta  # (m x k) = (m x (n + 1)) x ((n + 1) x k)
+            # Compute logits and apply softmax function
+            logits = X_with_bias @ self.Theta           # (m x k) = (m x (n + 1)) x ((n + 1) x k)
+            Y_proba_pred = self._softmax(logits)        # (m x k)
+            error = Y_proba_pred - Y_one_hot            # (m x k)
 
-            # Apply softmax function to logits
-            Y_proba_pred = self._softmax(logits)  # (m x k)
+            if epoch % 500 == 0:
+                print(epoch, self._compute_cost(X_with_bias, Y_one_hot))
 
-            error = Y_proba_pred - Y_one_hot  # (m x k)
-
-            # Apply different regularization based to regularized_type to compute current gradients
+            # Compute gradient of cost function
             if self.regularized_type == 'l2':   # l2 loss
                 gradients = (1 / m) * X_with_bias.T @ error + np.r_[np.zeros((1, self.K)), self.alpha * self.Theta[1:]]
             elif self.regularized_type == 'l1': # l1 loss
-                raise NotImplementedError()
+                gradients = (1 / m) * X_with_bias.T @ error + np.r_[np.zeros((1, self.K)), self.alpha * np.sign(self.Theta[1:])]
             else:   # Unregularized
                 gradients = (1 / m) * X_with_bias.T @ error  # ((n + 1) x k) = ((m x (n + 1))^T) x (m x k)
 
             # Update Theta
             self.Theta -= self.learning_rate * gradients  # ((n + 1) x k)
 
-            # If early_stopping is enable, stop iteration when the current cost is higher than previous cost
-            if self.early_stopping:
-                current_cost = self._compute_cost(X_with_bias, Y_one_hot)
 
-                if current_cost < best_cost:
-                    best_cost = current_cost
-                else:
-                    print(epoch, current_cost, 'early stopping!')
-                    break
+    def fit_with_early_stopping(self, X_train, y_train, X_valid, y_valid):
+        """
+        If fit the model with early stopping, batch gradient descent will stop as soon as 
+        the cost of validation set is higher than the previous cost of it
+        """
+
+        # Validate dimension of X and y
+        if X_train.ndim != 2 or X_valid.ndim != 2:
+            raise ValueError('X is expected to be a 2d numpy array')
+        elif y_train.ndim != 1 or y_valid.ndim != 1:
+            raise ValueError('y is expected to be a 1d numpy array')
+        elif len(X_train) != len(y_train) or len(X_valid) != len(y_valid):
+            raise ValueError('Number of rows of X must match with number of rows of y')
+
+        # Add bias term to X
+        X_train_w_bias = np.c_[np.ones((len(X_train), 1)), X_train]
+        X_valid_w_bias = np.c_[np.ones((len(X_valid), 1)), X_valid]
+
+        # One hot encode y
+        Y_train_one_hot = preprocessing.to_one_hot(y_train)
+        Y_valid_one_hot = preprocessing.to_one_hot(y_valid)
+
+        # Declare some useful variables
+        self.K = Y_train_one_hot.shape[1]                  # Number of classes
+        self.n = X_train_w_bias.shape[1] - 1               # Number of features
+        m = len(X_train)                                   # Number of training examples
+
+        # Initialize theta
+        self.Theta = np.random.randn(self.n + 1, self.K)
+
+        # Keep track of lowest cost of validation set
+        best_valid_cost = np.inf
+
+        for epoch in range(self.max_iter):
+            
+            # Compute Y_proba_pred
+            logits = X_train_w_bias @ self.Theta  # (m x k) = (m x (n + 1)) x ((n + 1) x k)
+            Y_proba_pred = self._softmax(logits)  # (m x k)
+            
+            # Compute error term
+            error = Y_proba_pred - Y_train_one_hot  # (m x k)
+
+            # Compute gradients of cost function
+            if self.regularized_type == 'l2':   # l2 loss
+                gradients = (1 / m) * X_train_w_bias.T @ error + np.r_[np.zeros((1, self.K)), self.alpha * self.Theta[1:]]
+            elif self.regularized_type == 'l1': # l1 loss
+                gradients = (1 / m) * X_train_w_bias.T @ error + np.r_[np.zeros((1, self.K)), self.alpha * np.sign(self.Theta[1:])]
+            else:   # Unregularized
+                gradients = (1 / m) * X_train_w_bias.T @ error
+
+            # Update Theta
+            self.Theta -= self.learning_rate * gradients  # ((n + 1) x k)
+
+            # Stop iteration when the current cost of validation set is higher than previous cost
+            current_cost = self._compute_cost(X_valid_w_bias, Y_valid_one_hot)
+
+            if epoch % 500 == 0:
+                print(epoch, current_cost)
+
+            if current_cost < best_valid_cost:
+                best_valid_cost = current_cost
+            else:
+                print(epoch, current_cost, 'early stopping!')
+                break
 
     def predict(self, X, return_in_prob=False):
+        """
+        Make prediction with input X using current available Theta
 
+        - Add bias to X
+        - Compute logits with current Theta
+        - Apply softmax function to logits to get the probabilities
+        - Either Return the Y_proba directly or get the class with highest prob then return
+        """
         if X.shape[1] != self.n:
             raise ValueError(f'Number of columns of X must equal to {self.n}')
 
@@ -111,6 +167,7 @@ class SoftmaxRegression:
 
         # Compute score s_k(X) for each class k for each example
         logits = X_with_bias @ self.Theta
+
         # estimate probability of each class by applying softmax function
         Y_proba_pred = self._softmax(logits)
 
@@ -118,32 +175,31 @@ class SoftmaxRegression:
             # The class with highest probability is the predicted class
             y_pred = np.argmax(Y_proba_pred, axis=1)
             return y_pred
+
         return Y_proba_pred
 
     def _compute_cost(self, X_with_bias, Y_one_hot):
         """
-        Compute cost of the model based on current theta and its respective cost function, cost function of SoftmaxRegression aka cross entropy
+        Compute cost of the model based on current theta and its respective cost function (cross entropy loss)
         """
-
-        m = len(X_with_bias)
+        
         logits = X_with_bias @ self.Theta
         Y_proba_pred = self._softmax(logits)
 
         # Add a tiny value epsilon to log(Y_proba_pred) to avoid getting nan values when Y_proba_pred contains any 0
         epsilon = 1e-7
 
+        cross_entropy_loss = -np.mean(np.sum(Y_one_hot * np.log(Y_proba_pred + epsilon), axis=1))
+
         if self.regularized_type == 'l2':
-            cross_entropy_loss = -np.mean(np.sum(Y_one_hot * np.log(Y_proba_pred + epsilon), axis=1))
             l2_loss = 1 / 2 * np.sum(np.square(self.Theta[1:]))
-            cost = cross_entropy_loss + self.alpha * l2_loss
+            cross_entropy_loss += self.alpha * l2_loss
 
         elif self.regularized_type == 'l1':
-            raise NotImplementedError()
+            l1_loss = np.sum(np.abs(self.Theta[1:]))
+            cross_entropy_loss += self.alpha * l1_loss
 
-        else:
-            cost = - (1 / m) * np.sum(Y_one_hot * np.log(Y_proba_pred + epsilon))
-
-        return cost
+        return cross_entropy_loss
 
     def _softmax(self, logits):
         """
--- just4funml/utils/preprocessing.py
+++ just4funml/utils/preprocessing.py
@@ -15,3 +15,24 @@ def to_one_hot(y):
         Y_one_hot[i, selected_col_index] = 1.0
 
     return Y_one_hot
+
+
+def train_validation_test_split(X, y, validation_ratio=0.2, test_ratio=0.2, random_state=None):
+    np.random.seed(random_state)
+    
+    total_size = len(X)
+
+    test_size = int(total_size * test_ratio)
+    validation_size = int(total_size * validation_ratio)
+    train_size = total_size - test_size - validation_size
+
+    rnd_indices = np.random.permutation(total_size)
+
+    X_train = X[rnd_indices[:train_size]]
+    y_train = y[rnd_indices[:train_size]]
+    X_valid = X[rnd_indices[train_size:-test_size]]
+    y_valid = y[rnd_indices[train_size:-test_size]]
+    X_test = X[rnd_indices[-test_size:]]
+    y_test = y[rnd_indices[-test_size:]]
+
+    return X_train, y_train, X_valid, y_valid, X_test, y_test
