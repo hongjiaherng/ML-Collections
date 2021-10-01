Fixed backprop bug

just4funml/algorithms/deep/initializers.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def weights_init(method, n_prev, n_this):
+def weights_init(method, n_prev, n_this, seed):
     if method == "he":
         # Good for ReLU activation
         # W^[l] ~ N(0, 2/n^[l-1]) 
@@ -13,10 +13,9 @@ def weights_init(method, n_prev, n_this):
         # Generally good
         # W^[l] ~ N(0, 2/(n^[l-1] + n^[l]))
         std = np.sqrt(2 / (n_prev + n_this))
-    elif method == "test":
-        return np.ones(shape=(n_prev, n_this))
     else:
         raise Exception(f"{method} initialization method doesn't exist")
 
+    np.random.seed(seed)
     return np.random.randn(n_prev, n_this) * std
 

just4funml/algorithms/deep/layers.py
@@ -49,6 +49,10 @@ class FullyConnected:
         dLdA_prev = np.dot(dLdZ, self.parameters["W"].T) # (m, n_prev) = (m, n_this) x (n_prev, n_this).T
         self.vol_in.grads[:] = dLdA_prev
 
+        # print(f"dJdW: {self.dJdW}")
+        # print(f"dJdb: {self.dJdb}")
+        # print(f"dLdA_prev: {self.vol_in.grads}")
+        
         # print(f"{self.layer_type} (out) \t-> {self.vol_out}")
         # print(f"{self.layer_type} (in) \t-> {self.vol_in}")
         
@@ -104,7 +108,7 @@ class Sigmoid:
         dAdZ = self.vol_out.tensor * (1 - self.vol_out.tensor)
         dLdZ = dLdA * dAdZ
         self.vol_in.grads[:] = dLdZ
-
+        # print(f"dLdZ: {self.vol_in.grads}")
         # print(f"{self.layer_type} (out) \t-> {self.vol_out}")
         # print(f"{self.layer_type} (in) \t-> {self.vol_in}")
         
@@ -119,7 +123,8 @@ class Tanh:
         self.vol_in = vol_in
         # Compute A = tanh(vol_in)
         Z = vol_in.tensor
-        A = (np.exp(2 * Z) - 1) / (np.exp(2 * Z) + 1)
+        exp_2z = np.exp(2 * Z)
+        A = (exp_2z - 1) / (exp_2z + 1)
         self.vol_out = Vol(A)
 
         return self.vol_out
@@ -151,16 +156,16 @@ class Regression:
         # y: (m, k)
         Y_hat = self.vol_in.tensor
 
-        m = Y.shape[0]
-        loss = 1 / (2 * m) * np.sum(np.power(Y_hat - Y, 2))
-
+        loss = 1 / 2 * np.power(Y_hat - Y, 2)
+        cost = np.mean(loss, axis=0, keepdims=True) 
+        
         dLdY_hat = Y_hat - Y
         self.vol_in.grads[:] = dLdY_hat
         
         # print(f"{self.layer_type} (out) \t-> {self.vol_out}")
         # print(f"{self.layer_type} (in) \t-> {self.vol_in}")
         
-        return loss
+        return np.squeeze(cost)
 
 
 class Logistic:
@@ -179,16 +184,17 @@ class Logistic:
         # Compute dLdA, given Y and Y_hat
         Y_hat = self.vol_in.tensor
 
-        m = Y.shape[0]
-        loss = -1 / m * np.sum(Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat))
+        loss = - (Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat))
+        cost = np.mean(loss, axis=0, keepdims=True)
         
-        dLdY_hat = (-Y / Y_hat) + (1 - Y / 1 - Y_hat)
+        dLdY_hat = (- Y / Y_hat) + ((1 - Y) / (1 - Y_hat))
         self.vol_in.grads[:] = dLdY_hat
-
+        # print(f"loss: {loss}")
+        # print(f"dLdA2: {self.vol_in.grads}")
         # print(f"{self.layer_type} (out) \t-> {self.vol_out}")
         # print(f"{self.layer_type} (in) \t-> {self.vol_in}")
 
-        return loss
+        return np.squeeze(cost)
 
 class Softmax:
     def __init__(self, n_neurons):

just4funml/algorithms/deep/net.py
@@ -44,13 +44,19 @@ class Net:
             else: raise Exception(proper_layers_config[i]["layer_type"] + " layer doesn't exist")
 
     
-    def init_params(self, method):
+    def init_params(self, method, seed=None):
+        check_params = {}
+        i = 1
         for l in range(len(self.layers)):
             if hasattr(self.layers[l], "parameters"):
-                self.layers[l].parameters["W"] = initializers.weights_init(method=method, n_prev=self.layers[l-1].n_neurons, n_this=self.layers[l].n_neurons)
+                self.layers[l].parameters["W"] = initializers.weights_init(method=method, n_prev=self.layers[l-1].n_neurons, n_this=self.layers[l].n_neurons, seed=seed)
                 self.layers[l].parameters["b"] = np.zeros((1, self.layers[l].n_neurons))
+                check_params[f"W{i}"] = self.layers[l].parameters["W"]
+                check_params[f"b{i}"] = self.layers[l].parameters["b"]
+                i += 1
         self.params_init = True
-        
+        return check_params
+
 
     def forward_prop(self, X):
         assert self.params_init
@@ -71,83 +77,3 @@ class Net:
         
         return loss
         
-
-layers_config = []
-# layers_config.append({"layer_type": "input", "n_neurons": 2})
-# layers_config.append({"layer_type": "fc", "n_neurons": 100, "activation": "relu"})
-# layers_config.append({"layer_type": "fc", "n_neurons": 100, "activation": "relu"})
-# layers_config.append({"layer_type": "fc", "n_neurons": 100, "activation": "relu"})
-# layers_config.append({"layer_type": "fc", "n_neurons": 100, "activation": "relu"})
-# layers_config.append({"layer_type": "softmax", "n_neurons": 2})
-
-# layers_config.append({"layer_type": "input", "n_neurons": 2})
-# layers_config.append({"layer_type": "fc", "n_neurons": 4})
-# layers_config.append({"layer_type": "relu"})
-# layers_config.append({"layer_type": "fc", "n_neurons": 4})
-# layers_config.append({"layer_type": "relu"})
-# layers_config.append({"layer_type": "regression", "n_neurons": 1})
-
-layers_config.append({"layer_type": "input", "n_neurons": 2})
-layers_config.append({"layer_type": "fc", "n_neurons": 4, "activation": 'relu'})
-# layers_config.append({"layer_type": "fc", "n_neurons": 10, "activation": 'relu'})
-layers_config.append({"layer_type": "logistic"})
-
-
-from sklearn.datasets import load_iris
-
-iris_dataset = load_iris()
-X = iris_dataset["data"]
-y = iris_dataset["target"]
-X_processed = X[:, 2:]
-Y_processed = (y == 0).astype('int').reshape(-1, 1)
-
-nn = Net()
-nn.bind_layers(layers_config)
-nn.init_params(method="xavier")
-
-Y_hat = nn.forward_prop(X_processed)
-loss = nn.backward_prop(Y_processed)
-Y_pred = (Y_hat > 0.5).astype("float")
-print("acc:", np.sum(Y_pred == Y_processed) / Y_processed.shape[0])
-print("loss:", loss)
-
-costs = []
-accs = []
-for i in range(5000):
-    Y_hat = nn.forward_prop(X_processed)
-    loss = nn.backward_prop(Y_processed)
-    
-    Y_pred = (Y_hat > 0.5).astype("float")
-    print(f"loss {i}: {np.round(loss, 3)}", end="\t")
-    acc = np.round(np.sum(Y_pred == Y_processed) / Y_processed.shape[0], 3)
-    print("acc:", acc)
-
-    costs.append(loss)
-    accs.append(acc)
-    
-    for layer in nn.layers:
-        if hasattr(layer, "parameters"):
-            layer.parameters["W"] = layer.parameters["W"] - 0.01 * layer.dJdW  
-            layer.parameters["b"] = layer.parameters["b"] - 0.01 * layer.dJdb
-
-
-import matplotlib.pyplot as plt
-
-xx, yy = np.meshgrid(
-                np.linspace(np.min(X_processed[:, 0]) - 1, np.max(X_processed[:, 0]) + 1, 500), 
-                np.linspace(np.min(X_processed[:, 1]) - 1, np.max(X_processed[:, 1]) + 1, 500)
-         )
-X_new = np.c_[xx.ravel(), yy.ravel()]
-y_proba = nn.forward_prop(X_new)
-y_predict = (y_proba > 0.5).astype("float")
-# zz1 = y_proba[:, 0].reshape(xx.shape)
-zz = y_predict.reshape(xx.shape)
-
-fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-# plt.contour(xx, yy, zz1)
-axes[0].contourf(xx, yy, zz, cmap="jet")
-axes[0].scatter(X_processed[:, 0], X_processed[:, 1], c=Y_processed)
-axes[1].plot(costs, label="cost")
-axes[1].plot(accs, label="acc")
-axes[1].legend()
-plt.show()

just4funml/algorithms/deep/test.py
@@ -1,11 +1,74 @@
 import numpy as np
+from net import Net 
+from sklearn.datasets import load_iris
 
-a = np.zeros((10, 10))
-print(id(a))
-print(a)
-print(id(a[0, 0]))
+# Load iris dataset
+iris_dataset = load_iris()
+X = iris_dataset["data"][:, 2:]
+y = iris_dataset["target"]
+y = (y == 0).astype('int').reshape(-1, 1)
 
-a[:] = np.ones((5, 5))
-print(id(a))
-print(a)
-print(id(a[0, 0]))
+# My own neural network :)
+layers_config = []
+# layers_config.append({"layer_type": "input", "n_neurons": 2})
+# layers_config.append({"layer_type": "fc", "n_neurons": 100, "activation": "relu"})
+# layers_config.append({"layer_type": "fc", "n_neurons": 100, "activation": "relu"})
+# layers_config.append({"layer_type": "fc", "n_neurons": 100, "activation": "relu"})
+# layers_config.append({"layer_type": "fc", "n_neurons": 100, "activation": "relu"})
+# layers_config.append({"layer_type": "softmax", "n_neurons": 2})
+
+# layers_config.append({"layer_type": "input", "n_neurons": 2})
+# layers_config.append({"layer_type": "fc", "n_neurons": 4})
+# layers_config.append({"layer_type": "relu"})
+# layers_config.append({"layer_type": "fc", "n_neurons": 4})
+# layers_config.append({"layer_type": "relu"})
+# layers_config.append({"layer_type": "regression", "n_neurons": 1})
+
+layers_config.append({"layer_type": "input", "n_neurons": 2})
+layers_config.append({"layer_type": "fc", "n_neurons": 4, "activation": 'sigmoid'})
+# layers_config.append({"layer_type": "fc", "n_neurons": 10, "activation": 'relu'})
+layers_config.append({"layer_type": "logistic"})
+
+nn = Net()
+nn.bind_layers(layers_config)
+check_params = nn.init_params(method="xavier", seed=42)
+check_X = X[:4]
+check_y = y[:4]
+
+# Manual check for forward propagation
+y_proba = nn.forward_prop(check_X)
+
+Z1 = np.dot(check_X, check_params["W1"]) + check_params["b1"]
+A1 = 1 / (1 + np.exp(-Z1))
+Z2 = np.dot(A1, check_params["W2"]) + check_params["b2"]
+A2 = 1 / (1 + np.exp(-Z2))
+# print(y_proba)
+# print(A2)
+
+# Manual check for backward propagation
+print("Backprop model")
+cost = nn.backward_prop(check_y)
+
+print("\nManual Backprop")
+loss_manual = - (check_y * np.log(A2) + (1- check_y) * np.log(1 - A2))
+cost_manual = np.squeeze(np.mean(loss_manual, axis=0))
+print(f"loss: {loss_manual}")
+dLdA2 = (- check_y / A2) + ((1 - check_y) / (1 - A2))
+print(f"dLdA2: {dLdA2}")
+dLdZ2 = dLdA2 * (1 - A2) * A2
+print(f"dLdZ2: {dLdZ2}")
+dJdW2 = 1 / 4 * np.dot(A1.T, dLdZ2)
+print(f"dLdW2: {dJdW2}")
+dJdb2 = 1 / 4 * np.sum(dLdZ2, axis=0, keepdims=True)
+print(f"dJdb2: {dJdb2}")
+dLdA1 = np.dot(dLdZ2, check_params["W2"].T)
+print(f"dLdA1: {dLdA1}")
+
+dLdZ1 = dLdA1 * (1 - A1) * A1
+print(f"dLdZ1: {dLdZ1}")
+dJdW1 = 1 / 4 * np.dot(check_X.T, dLdZ1)
+print(f"dJdW1: {dJdW1}")
+dJdb1 = 1 / 4 * np.sum(dLdZ1, axis=0, keepdims=True)
+print(f"dJdb1: {dJdb1}")
+dLdA0 = np.dot(dLdZ1, check_params["W1"].T) 
+print(f"dLdA0: {dLdA0}")